55484e21f3f51bef1a8acf0632bc25668e42c27e
Kangwoo Jung <kjung@c3r8s6.42gyeongsan.kr>
2024-07-31 14:44:51 +0900

    test

--- srcs/utils/ms_parcing.c
+++ srcs/utils/ms_parcing.c
@@ -6,9 +6,13 @@
 /*   By: kjung <kjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 14:22:47 by kjung             #+#    #+#             */
-/*   Updated: 2024/07/31 14:22:59 by kjung            ###   ########.fr       */
+/*   Updated: 2024/07/31 14:42:49 by kjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void	ft_parcing(char **av)
+{
+		
+}
