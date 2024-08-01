686a97238fc3af66817d7324361a44149cfa3935
Jaehun Kim <jaehukim@c3r8s6.42gyeongsan.kr>
2024-08-01 11:07:49 +0900

    prompt1

--- srcs/ms_prompt.c
+++ srcs/ms_prompt.c
@@ -9,5 +9,16 @@
 /*   Updated: 2024/07/31 16:06:54 by jaehukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "../includes/minishell.h"
 
+void	ft_prompt(void)
+{
+	char	*input;
+	char	*shell_prompt;
 
+	while (1)
+	{
+		shell_prompt = getenv("USER");
+		input = readline(shell_prompt);
+	}
+}
