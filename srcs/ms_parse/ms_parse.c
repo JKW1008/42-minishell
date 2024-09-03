b66d2eb15ee692296152f4c7f631a861930d4dc2
Jaehun Kim <jaehukim@c3r8s5.42gyeongsan.kr>
2024-09-03 17:47:28 +0900

    asdf

--- srcs/ms_parse/ms_parse.c
+++ srcs/ms_parse/ms_parse.c
@@ -12,59 +12,6 @@
 
 #include "../../includes/minishell.h"
  
-t_ast_node	*create_node(t_node_type type, t_token *tkn)
-{
-	t_ast_node	*node;
-	
-	node = malloc(sizeof(t_ast_node));
-	if (!node)
-	{
-		write(STDERR_FILENO, "Allocation Failed\n", 19);
-		exit(EXIT_FAILURE);
-	}
-	node->type = type;
-	node->value = ft_strdup(tkn->value);
-	node->tkn_idx = tkn->tkn_idx;
-	node->qt_status = tkn->qt_status;
-	node->left = NULL;
-	node->right = NULL;
-	return (node);
-}
-
-t_ast_node	*ft_find_pipe(t_data **data, int num)
-{
-	t_token		*current;
-
-	current = (*data)->tkn->head;
-	while (current->tkn_idx < num)
-		 current = current->next;
-	while (current || current->token_type != l_pipe)
-		current = current->next;
-	if (!current)
-		return (NULL);
-	else
-		return (create_node(NODE_PIPE, current));
-}
-
-t_ast_node ast_command(t_data **data)
-{
-
-}
-
-int ft_creat_left(t_data **data, t_ast_node *head)
-{
-	return (1);
-}
-
-t_ast_node	*parse(t_data **data)
-{
-	t_ast_node *root;
-
-	head_node = ft_find_pipe(data, 0);
-	if (head_node)
-		;
-
-}
 
 size_t	ft_parser(t_data **data)
 {
