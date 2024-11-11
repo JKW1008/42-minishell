6fa502e72bb54d7ce90a61cd4a5e1c500f7e1abf
Kangwoo Jung <kjung@c2r16s2.42gyeongsan.kr>
2024-11-11 17:06:55 +0900

    sdf

--- srcs/ms_prompt.c
+++ srcs/ms_prompt.c
@@ -6,7 +6,7 @@
 /*   By: kjung <kjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 16:06:53 by jaehukim          #+#    #+#             */
-/*   Updated: 2024/10/17 18:56:11 by kjung            ###   ########.fr       */
+/*   Updated: 2024/11/11 17:06:16 by kjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ void	ft_prompt(t_data **data)
 	ft_memset(&heredoc_list, 0, sizeof(t_heredoc_list));
 	while (1)
 	{
-
 		input = readline("$ ");
 		if (!input)
 			break ;
