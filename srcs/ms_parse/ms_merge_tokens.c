702c786e19981f7488d50234322d2f68f384fd02
Jaehun Kim <jaehukim@c2r7s7.42gyeongsan.kr>
2024-11-12 17:47:48 +0900

    [Feat] Merge Tokens

--- srcs/ms_parse/ms_merge_tokens.c
+++ srcs/ms_parse/ms_merge_tokens.c
@@ -78,7 +78,8 @@ size_t	ft_merge_tokens(t_data **data)
 	tkn = (*data)->tkn->head;
 	while (tkn)
 	{
-		if (tkn->pre_sep != ' ' && tkn->tkn_idx > 0)
+		if (tkn->pre_sep != ' ' && tkn->prev && \
+			(tkn->prev->qt_status != normal || tkn->qt_status != normal))
 			tkn = merge_tokens(tkn, prev);
 		if (tkn->next)
 		{
