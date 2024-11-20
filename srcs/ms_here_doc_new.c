cefe14fae846e3374da4f9379db14d9f2d5d7cd0
Jaehun Kim <jaehukim@c2r17s4.42gyeongsan.kr>
2024-11-20 16:14:19 +0900

    [Feat] Heredoc Signal & dup

--- srcs/ms_here_doc_new.c
+++ srcs/ms_here_doc_new.c
@@ -57,6 +57,7 @@ static void process_heredoc_new(t_cmd *cmd, int i)
 	if (pid == 0)
 	{
 		signal(SIGINT, heredoc_sighandler);
+		signal(SIGQUIT, SIG_IGN);
 		ft_heredoc_gnl(cmd, i, fd);
 		exit(EXIT_SUCCESS);
 	}
@@ -65,7 +66,8 @@ static void process_heredoc_new(t_cmd *cmd, int i)
 		signal(SIGINT, SIG_IGN);
 		waitpid(pid, &status, 0);
 		close(fd[1]);
-		cmd->rdr[i]->fd = fd[0];
+		dup2(STDIN_FILENO, fd[0]);
+		cmd->rdr[i]->fd = STDIN_FILENO;
 	}
 	ft_ctrl_signal();
 }
